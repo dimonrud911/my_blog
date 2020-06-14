4529aa05bed7fc12810f24a9567676c34e6c9311
dimon.rudenskyi <dimon.rudenskyi@gmail.com>
2020-06-15 00:53:54 +0300

    Change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
+user = {
+  email: "admin@mail.com",
+  password: "11111111",
+  passowrd_confirmation: "11111111",
+  first_name: "Admin",
+  last_name: "Blog"
+}
 
 categories = [
   {
@@ -14,6 +21,13 @@ categories = [
     description: 'All you need to know about social media'
   }
 ]
+
+first_user = User.create!(email: user[:email],
+            password: user[:password], 
+            passowrd_confirmation: user[:passowrd_confirmation], 
+            first_name: user[:first_name], 
+            last_name: user[:last_name])
+
 categories.each do |category|
-  Category.create!(name: category[:name], description: category[:description])
+  Category.create!(name: category[:name], description: category[:description], user_id: first_user.id)
 end
